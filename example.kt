e69a7dc42d67decf4e039088c29dae7ce50fbfbe
Loobo45 <maxi_testa2012@hotmail.com>
2023-11-14 20:26:23 -0300

    kotlin is fun

--- example.kt
+++ example.kt
@@ -1,6 +1,19 @@
+import java.util.*
 fun main(){
-    var numbers: Array<Int> = arrayOf(11,12,13,14,15)
-    var list: MutableList<String> = mutableListOf()
-    for (element in numbers) list.add(element.toString())
-    println(list)
+    semana()
+}
+fun semana(){
+    println("Que dia es hoy?")
+    val dia = Calendar.getInstance().get(Calendar.DAY_OF_WEEK)
+    println(when (dia) {
+        1 -> "Domingo"
+        2 -> "Lunes"
+        3 -> "Martes"
+        4 -> "Miercoles"
+        5 -> "Jueves"
+        6 -> "Viernes"
+        7 -> "Sabado"
+        else -> "GGWP hora de suicidarse no funciona esto"
+    }
+    )
 }

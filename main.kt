c3f18077fe9284f84877be78036758b53536b767
Loobo45 <maxi_testa2012@hotmail.com>
2023-11-15 08:37:25 -0300

    more kotlin tests

--- main.kt
+++ main.kt
@@ -1,12 +1,14 @@
 fun main(){
-    val fortuna = fortuna()
-    print("Tu fortuna es: $fortuna")
+    println(addFish(10.0,listOf(3,3,3)).toString())
+    println(addFish(8.0,listOf(2,2,2),hasDecoration = false).toString())
+    println(addFish(9.0,listOf(1,1,3),3).toString())
+    println(addFish(10.0,listOf(),7,true).toString())
 }
 
-fun fortuna(): String{
-    val lista: List<String> = listOf("sexo","follar","coito","reproduccion","metesaca","intimidad","mucho sexo")
-    print("cuando cumplis mi pana? ")
-    var cumple = 14
-    return lista[cumple/7]
-
+fun addFish(tankSize: Double, currentFish: List<Int>, fishSize: Int = 2, hasDecoration: Boolean = true): Boolean{
+    var totalFish = 0
+    val realTankSize = if (hasDecoration == true) tankSize*0.8 else tankSize
+    for (element in currentFish) totalFish += element
+    println("$realTankSize, $totalFish")
+    return if ((totalFish + fishSize)>realTankSize) false else true
 }

be330690ceb578972b97f92e42b5b10307b8d658
IvanCenyko <ivancenyko@gmail.com>
2025-04-08 19:31:10 -0300

    Add endpoints to set and retrieve state with (very) simple password protection

--- app.py
+++ app.py
@@ -10,3 +10,19 @@ app = FastAPI()
 # Read the parameter from the .env file
 CONTRA = os.getenv("CONTRA")
 
+estado_actual = {"state": False}
+
+# USO: url/set?valor=<bool>&password=<password>
+# retorna que la operacion se hizo adecuadamente
+@app.post("/set")
+def set(valor: bool, password:str):
+    if password != CONTRA:
+        raise HTTPException(status_code=401, detail="Password incorrecta.")
+    estado_actual["state"] = valor
+    return {"status": True}
+
+# USO: url/
+# retorna estado actual
+@app.get("/status")
+def status():
+    return {"state": estado_actual["state"]}
